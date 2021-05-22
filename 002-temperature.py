49e83b33c78d27067791fde0b4f5556c583f56a4
Quenio <quenio@hey.com>
2021-05-22 20:33:13 -0300

    Update 002-temperature.py

--- 002-temperature.py
+++ 002-temperature.py
@@ -1,3 +1,5 @@
+# (C) 2021 Quenio. Licensed under CC0 1.0: https://creativecommons.org/publicdomain/zero/1.0
+
 fahrenheit = 0
 celsius = (fahrenheit - 32) * 0.55
 print(f"{fahrenheit:.1f}f -> {celsius:.1f}c")
