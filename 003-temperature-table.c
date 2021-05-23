8fdb4a16d47ffb24ed3612562ea5c1aaeca05cd1
Quenio <quenio@hey.com>
2021-05-22 21:02:43 -0300

    Update 003-temperature-table.c

--- 003-temperature-table.c
+++ 003-temperature-table.c
@@ -7,6 +7,6 @@ int main()
   for (float fahrenheit = 0; fahrenheit <= 100; fahrenheit += 10)
   {
     float celsius = (fahrenheit - 32) * 0.55;
-    printf("%.1ff -> %.1fc\n", fahrenheit, celsius);
+    printf("%3.1ff -> %3.1fc\n", fahrenheit, celsius);
   }
 }
