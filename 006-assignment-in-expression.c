e10458fae993f97a98c13ba9722fcef13f744165
Quenio <quenio@hey.com>
2021-05-23 03:28:38 +0000

    Made more concise with assignment in expression.

---
+++ 006-assignment-in-expression.c
@@ -0,0 +1,14 @@
+// (C) 2021 Quenio. Licensed under CC0 1.0: https://creativecommons.org/publicdomain/zero/1.0
+
+#include <stdio.h>
+
+int main()
+{
+  int c;
+  while ((c = getchar()) != EOF)
+  {
+    putchar(c);
+    putchar(c);
+  }
+}
+
