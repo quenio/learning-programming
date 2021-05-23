674f5701401d4541ac7d14218db00442c2f57811
Quenio <quenio@hey.com>
2021-05-22 21:53:37 -0300

    Update 003-looping.py

--- 003-looping.py
+++ 003-looping.py
@@ -1,5 +1,5 @@
 # (C) 2021 Quenio. Licensed under CC0 1.0: https://creativecommons.org/publicdomain/zero/1.0
 
-for fahrenheit in range(0, 110, 10):
+for fahrenheit in range(start=0, srop=110, step=10):
     celsius = (fahrenheit - 32) * 0.55
     print(f"{fahrenheit:3.0f}F -> {celsius:5.1f}C")
