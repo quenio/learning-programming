2d779a0582020fc8ce4e3f5393c891bad2c16327
Quenio <quenio@hey.com>
2021-06-04 19:43:07 +0000

    Inserted the license header.

--- 002-calculating.fsx
+++ 002-calculating.fsx
@@ -1,3 +1,7 @@
+// (C) 2021 Quenio. Licensed under CC0 1.0: https://creativecommons.org/publicdomain/zero/1.0
+
 let fahrenheit = 0.0
 let celsius = (fahrenheit - 32.0) * 0.55
+
 printfn $"{fahrenheit}F -> {celsius}C"
+
--- 003-looping.fsx
+++ 003-looping.fsx
@@ -1,4 +1,7 @@
+// (C) 2021 Quenio. Licensed under CC0 1.0: https://creativecommons.org/publicdomain/zero/1.0
+
 for i = 0 to 10 do
   let fahrenheit = 10.0 * (float i)
   let celsius = (fahrenheit - 32.0) * 0.55
   printfn "%3.0fF -> %5.1fC" fahrenheit celsius
+
